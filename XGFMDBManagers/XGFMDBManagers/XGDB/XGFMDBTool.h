8ddcc87afa71ba7fd9dacb6e42cfede2179269c7
赵晓楠 <zhaoxiaonan@3ikids.com>
2015-08-30 11:03:48 +0800

    完善注释

--- XGFMDBManagers/XGFMDBManagers/XGDB/XGFMDBTool.h
+++ XGFMDBManagers/XGFMDBManagers/XGDB/XGFMDBTool.h
@@ -9,26 +9,33 @@
 #import <Foundation/Foundation.h>
 #import "XGKeyValueItem.h"
 /*
-    思路:
-                这个数据库 思路 借鉴了 猿题库开源数据库https://github.com/yuantiku/YTKKeyValueStore Key  Value的思路
-    遇到的问题:  
-                在使用时遇到查询某一类型的数据时 YTKKeyValueStore 数据表格 只留了 时间 和id 以供查询
-                显而易见 这样的数据库结构极不方便条件查询 
-                在XGKeyValueStore中进行了 优化
+ 思路:
  
-    存储数据种类:
-                YTKKeyValueStore 所存储的 数据类型 有限
-                XGKeyValueStore  只要给数据 就能存  (字典 模型 字符串等)任意存任意取 并且模型不用遵守NSCoding协议
+             这个数据库 思路 借鉴了 猿题库开源数据库https://github.com/yuantiku/YTKKeyValueStore Key  Value的思路
  
-    缺点:
-                XGKeyValueStore 暂时支持存的 是什么东西 数据库返回的就是什么东西  不会对数据进行处理(如果存的是json 数据库不会处理)
+ 遇到的问题:
  
-    如何使用: 
+             在使用时遇到查询某一类型的数据时 YTKKeyValueStore 数据表格 只留了 时间 和id 以供查询
+             显而易见 这样的数据库结构极不方便条件查询
+             在XGKeyValueStore中进行了 优化
  
-        使用 这个数据库需要引入XGDB文件夹 中的 几个类
+ 存储数据种类:
  
-        对外使用 只需用XGFMDBTool 就OK  查询 数据 返回的数据都是XGKeyValueItem 模型 其中的itemResult 就是所存储的数据
-        id  pos 必传      pos(辅助主键) 可以读取数据时排序使用
+             XGKeyValueStore  只要给数据 就能存  (字典 模型 字符串等)任意存任意取 并且模型不用遵守NSCoding协议
+ 
+ 缺点:
+ 
+             XGKeyValueStore 暂时支持  存的是什么东西  数据库返回的就是什么东西  不会对数据进行处理(如果存的是json 数据库不会处理)
+ 
+ 如何使用:
+ 
+             使用 这个数据库需要依赖FMDB第三方库  并且引入XGDB文件夹 中的 几个类
+             
+             对外使用 只需用XGFMDBTool 就OK
+             
+             查询数据 返回的数据都是XGKeyValueItem 模型 其中的itemResult 就是所存储的数据
+
+             id  pos 必传      pos(辅助主键) 可以读取数据时排序使用
  
  */
 
